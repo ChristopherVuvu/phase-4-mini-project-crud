e78f295b58c77cb4fa4e3e90c69bc1c5196c43fe
ChristopherVuvu <vuvumumo93@gmail.com>
2023-06-29 18:50:30 +0300

    added

--- spec/models/spice_spec.rb
+++ spec/models/spice_spec.rb
@@ -1,49 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Spice, type: :model do
-  let!(:spice) do 
-    Spice.create(
-      title: "Allspice Berries, Whole (Jamaican)",
-      image: "https://www.spicejungle.com/pub/media/catalog/product/cache/3db4d5004662ba3673dd7a19a8603593/w/h/whole-allspice-berries.jpg",
-      description: "Allspice has a curious name, doesn't it? Allspice. It's because it smells like so many other spices all smashed into one. Cinnamon, clove, cardamom.... all the best in one little berry!",
-      notes: "Pungent, Sweet, Warm & Earthy",
-      rating: 3.5
+  it "is valid with a title" do
+    spice = Spice.new(title: "Cinnamon")
+    expect(spice).to be_valid
+  end
+
+  it "is valid with all attributes" do
+    spice = Spice.new(
+      title: "Caraway Seeds",
+      image: "https://cdn.pixabay.com/photo/2018/05/16/05/11/seeds-3404903_1280.jpg",
+      description: "A crusty grandmother. She walked fifteen miles to school every day. Uphill! Both ways! In the snow! She’s cantankerous, but then she has those moments where she makes you tea and homemade bread and enlightens you about the little things in life.",
+      notes: "Bitter or Astringent, Sweet",
+      rating: 2
     )
-  end
-
-  describe '#id' do
-    it 'has an id' do
-      expect(spice.id).not_to eq(nil)
-    end
-  end
-
-  describe '#title' do
-    it 'returns the spice\'s title' do
-      expect(spice.title).to eq('Allspice Berries, Whole (Jamaican)')
-    end
-  end
-  
-  describe '#image' do
-    it 'returns the spice\'s image' do
-      expect(spice.image).to eq('https://www.spicejungle.com/pub/media/catalog/product/cache/3db4d5004662ba3673dd7a19a8603593/w/h/whole-allspice-berries.jpg')
-    end
-  end
-
-  describe '#description' do
-    it 'returns the spice\'s description' do
-      expect(spice.description).to eq('Allspice has a curious name, doesn\'t it? Allspice. It\'s because it smells like so many other spices all smashed into one. Cinnamon, clove, cardamom.... all the best in one little berry!')
-    end
-  end
-
-  describe '#notes' do
-    it 'returns the spice\'s notes' do
-      expect(spice.notes).to eq('Pungent, Sweet, Warm & Earthy')
-    end
-  end
-
-  describe '#rating' do
-    it 'returns the spice\'s rating' do
-      expect(spice.rating).to eq(3.5)
-    end
+    expect(spice).to be_valid
   end
 end
